f5e39a50adf9e7576488932276728943cf0d1757
Aaron <aaronsweeneydev@gmail.com>
2023-07-02 14:52:57 +0100

    Testing successful fetch

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Home from './components/Home';
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+var mock = new MockAdapter(axios);
+
+mock.onGet('http://localhost:3001/trips').reply(200, [
+  {
+    id: 1,
+    title: 'European Quest',
+    countries: 1,
+    days: 2,
+    emissionsValue: 10,
+    emissionsUnit: 'kg',
+    rating: 5,
+    imageURL: 'http://test.com',
+  },
+]);
+
+test('fetches and displays data', async () => {
+  render(<Home />);
+  await waitFor(() => expect(screen.getByText('European Quest')).toBeInTheDocument());
 });
