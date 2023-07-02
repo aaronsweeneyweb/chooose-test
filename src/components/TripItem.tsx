f5af1169301705f6773135ba9e6e639a2c43708c
Aaron <aaronsweeneydev@gmail.com>
2023-07-02 15:32:41 +0100

    Adding stars

--- src/components/TripItem.tsx
+++ src/components/TripItem.tsx
@@ -1,4 +1,13 @@
 import { TripItemProps } from '../types';
+import StarRatings from 'react-star-ratings';
+
+const STAR_CONFIG = {
+  color: '#F9D110',
+  total: 5,
+  name: 'rating',
+  dimension: '20px',
+  spacing: '0px'
+}
 
 const TripItem = (item: TripItemProps) => {
 
@@ -7,6 +16,14 @@ const TripItem = (item: TripItemProps) => {
       <div>{item.title}</div>
       <div>{item.countries}</div>
       <div>{item.days}</div>
+      <StarRatings
+        rating={item.rating}
+        starRatedColor={STAR_CONFIG.color}
+        numberOfStars={STAR_CONFIG.total}
+        name={STAR_CONFIG.name}
+        starDimension={STAR_CONFIG.dimension}
+        starSpacing={STAR_CONFIG.spacing}
+      />
       <div>{item.rating}</div>
     </div>
   )
